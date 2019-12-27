62fcdceafcfb2669389f2c70f1f48ffc50013b9a
Bryn Heath <brynheath1@gmail.com>
2019-12-27 20:10:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -2,12 +2,12 @@ def create_an_empty_array
   []
 end
 
-def create_an_array["one","two","three","four"]
-  
+def create_an_array
+  ["one","two","three","four"]
 end
 
 def add_element_to_end_of_array(array, element)
- return array<<element
+ return array << element
 end
 
 def add_element_to_start_of_array(array, element)
